0d814a308d718d161f886b060e14be17361d8dd4
Sanna Olsson <saol1901@student.miun.se>
2024-05-16 21:42:15 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'courses', component: CoursesComponent},
+    {path:'schedule', component: ScheduleComponent},
+    {path:'', redirectTo: '/home', pathMatch: 'full'},
+    {path:'404', component: NotFoundComponent},
+    {path:'**', component: NotFoundComponent}
+];
